2cf620d951e05b77414c6e271e66cd8fd1ea32b0
pashmash372 <abhilashsivasanker@gmail.com>
2023-05-29 18:14:06 +0530

    added JSONPlaceholderAPI.java and did changes in main for args result
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- java_basics1/src/main/java/org/example/Main.java
+++ java_basics1/src/main/java/org/example/Main.java
@@ -1,15 +1,42 @@
 package org.example;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println(Lib.getGreeting());
+//        Arrays.stream(args).forEach((Consumer<? super String>) System.out::println);
+        if (args.length == 0) {
+            System.out.println(Lib.getGreeting());
+        } else {
+            if (args[0].equals("example")) {
+                var httpLib = new HttpLib();
+                System.out.println(httpLib.getExampleDotCom());
+            }
+            if (args[0].equals("posts")) {
 
-        var httpLib= new HttpLib();
-        System.out.println(httpLib.getExampleDotCom());
+                var api = JSONPlaceholderAPI.getInstance();
+
+                api.getPosts().execute().body().forEach(post -> {
+                    System.out.println(post.getTitle());
+                });
+            }
+
+            if (args[0].equals("photos")) {
+
+                var api = JSONPlaceholderAPI.getInstance();
+
+                api.getPhotos().execute().body().forEach(photo -> {
+                    System.out.println(photo.getUrl());
+                });
+            }
+        }
     }
 }
